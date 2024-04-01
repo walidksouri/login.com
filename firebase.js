9db66b6ffa42b4e700cdc3001c774e2dc0af3684
walidksouri <164755969+walidksouri@users.noreply.github.com>
2024-04-02 00:04:25 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+
+  // Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCFy6eXPVXcTkI_FLuetKeNVEmKKz4n6Bw",
+  authDomain: "new-project-fa29a.firebaseapp.com",
+  projectId: "new-project-fa29a",
+  storageBucket: "new-project-fa29a.appspot.com",
+  messagingSenderId: "778990801029",
+  appId: "1:778990801029:web:4cc4db3a4059d943a699ec",
+  measurementId: "G-2GQZ30XRMK"
+};
+
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
